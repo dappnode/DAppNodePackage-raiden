0cf27233aab32d2e4507655edb55473839ac788d
vdo <vdo@greyfaze.net>
2019-03-26 15:09:20 +0100

    Revert to root, use vars

--- build/init.sh
+++ build/init.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ ! -z $RAIDEN_ADDRESS ] && [ -f ./password ] && [ `ls -1A keystore | wc -l` -gt 0 ]; then
-    /usr/bin/raiden --accept-disclaimer --environment-type production --api-address 0.0.0.0:80 --eth-rpc-endpoint $RAIDEN_RPC --keystore-path /home/raiden/keystore --address $RAIDEN_ADDRESS --password-file password
+if [ ! -z ${RAIDEN_ADDRESS} ] && [ -f ${RAIDEN_PASSWORD_FILE} ] && [ `ls -1A ${RAIDEN_KEYSTORE_PATH} | wc -l` -gt 0 ]; then
+        raiden --address ${RAIDEN_ADDRESS}
 else
     echo "########################################################"
     echo "## Raiden not properly configured!                    ##"
     echo "## Make sure you have done the following steps:       ##"
-    echo "## 1: Uploaded a keystore to /home/raiden/keystore/   ##"
+    echo "## 1: Uploaded a keystore to /root/.raiden/keystore/  ##"
     echo "## 2: Uploaded a password file, named 'password'      ##"
     echo "## 3: Set the corresponding \$RAIDEN_ADDRESS variable  ##"
     echo "## 4: Restart the package                             ##"
