6b400bb5561cf79103d9cd17c71a4cade4905a99
vdo <vdo@greyfaze.net>
2019-03-28 09:54:09 +0100

    Use expect for pw input.

--- build/init.sh
+++ build/init.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-if [ ! -z ${RAIDEN_ADDRESS} ] && [ -f ${RAIDEN_PASSWORD_FILE} ] && [ `ls -1A ${RAIDEN_KEYSTORE_PATH} | wc -l` -gt 0 ]; then
-        raiden --address ${RAIDEN_ADDRESS}
+# Check if password is set, and at least one file has been uploaded
+if [ ! -z ${RAIDEN_KEYSTORE_PASSWORD} ] && [ `find . -maxdepth 1 -type f | wc -l` -gt 0 ]; then
+    expect -c  "spawn raiden --keystore-path . --accept-disclaimer; 
+                sleep 3; 
+                expect -re \"password\";
+                send \"${RAIDEN_KEYSTORE_PASSWORD}\r\n\";
+                set timeout -1;
+                expect -re \"100%\";"
 else
+    echo
     echo "########################################################"
-    echo "## Raiden not properly configured!                    ##"
-    echo "## Make sure you have done the following steps:       ##"
-    echo "## 1: Uploaded a keystore to /root/.raiden/keystore/  ##"
-    echo "## 2: Uploaded a password file, named 'password'      ##"
-    echo "## 3: Set the corresponding \$RAIDEN_ADDRESS variable  ##"
-    echo "## 4: Restart the package                             ##"
+    echo "## Raiden not configured to start!                    ##"
+    echo "## Please follow the following steps:                 ##"
+    echo "## 1: Upload a keystore file                          ##"
+    echo "## 2: Set the RAIDEN_KEYSTORE_PASSWORD to open it     ##"
+    echo "## 3: Restart the package                             ##"
     echo "########################################################"
-    while true; do sleep 2; done
+    while true; do sleep 5; done
 fi
