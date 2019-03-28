257da25cc084a7c08ff5c4204f35256c9178e93d
vdo <vdo@greyfaze.net>
2019-03-28 16:55:40 +0100

    Select address

--- build/init.sh
+++ build/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check if password is set, and at least one file has been uploaded
-if [ ! -z ${RAIDEN_KEYSTORE_PASSWORD} ] && [ `find . -maxdepth 1 -type f | wc -l` -gt 0 ]; then
+if [ ! -z ${RAIDEN_ADDRESS} ] && [ ! -z ${RAIDEN_KEYSTORE_PASSWORD} ] && [ `find . -maxdepth 1 -type f | wc -l` -gt 0 ]; then
     expect -c  "spawn raiden --keystore-path . --accept-disclaimer; 
                 sleep 3; 
                 expect -re \"password\";
@@ -13,7 +13,8 @@ else
     echo "########################################################"
     echo "## Raiden not configured to start!                    ##"
     echo "## Please follow the following steps:                 ##"
-    echo "## 1: Upload a keystore file                          ##"
+    echo "## 1: Upload a keystore file to /root/.raiden         ##"
+    echo "## 2: Set the RAIDEN_ADDRESS of the keystore          ##"
     echo "## 2: Set the RAIDEN_KEYSTORE_PASSWORD to open it     ##"
     echo "## 3: Restart the package                             ##"
     echo "########################################################"
