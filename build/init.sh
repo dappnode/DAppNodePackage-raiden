7df1835e1cff4454694d212b16d9f6704ec2df0f
vdo <vdo@greyfaze.net>
2019-06-12 22:03:42 +0200

    Add extra opts

--- build/init.sh
+++ build/init.sh
@@ -4,7 +4,7 @@ set -e
 # Check if password is set, and at least one file has been uploaded
 if [ -n "${RAIDEN_ADDRESS}" ] && [ -n "${RAIDEN_KEYSTORE_PASSWORD}" ] && [ "$(find . -maxdepth 1 -type f | wc -l)" -gt 0 ]; then
     echo "${RAIDEN_KEYSTORE_PASSWORD}" > .password
-    raiden --keystore-path . --accept-disclaimer --password-file .password
+    raiden --keystore-path . --accept-disclaimer --password-file .password "${EXTRA_OPTS}"
 else
     echo
     echo "########################################################"
