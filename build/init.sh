6317a4e6104acd887ba2f0add1d7236ad6a4495c
vdo <vdo@greyfaze.net>
2020-06-02 19:02:38 +0200

    move to 1.0.0, add wizard

--- build/init.sh
+++ build/init.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 set -e
 
-# Check if password is set, and at least one file has been uploaded
-if [ -n "${RAIDEN_ADDRESS}" ] && [ -n "${RAIDEN_KEYSTORE_PASSWORD}" ] && [ "$(find . -maxdepth 1 -type f | wc -l)" -gt 0 ]; then
-    echo "${RAIDEN_KEYSTORE_PASSWORD}" > .password
-    raiden --keystore-path . --accept-disclaimer --password-file .password "${EXTRA_OPTS}"
-else
-    echo
-    echo "########################################################"
-    echo "## Raiden not configured to start!                    ##"
-    echo "## Please follow the following steps:                 ##"
-    echo "## 1: Upload a keystore file to /root/.raiden         ##"
-    echo "## 2: Set the RAIDEN_ADDRESS of the keystore          ##"
-    echo "## 2: Set the RAIDEN_KEYSTORE_PASSWORD to open it     ##"
-    echo "## 3: Restart the package                             ##"
-    echo "########################################################"
+function sleep_forever() {
     while true; do sleep 5; done
+}
+KEYSTORE_PATH="/root/.raiden"
+KEYSTORE_FILE="${KEYSTORE_PATH}/keystore.json"
+
+if [ ! -f "${KEYSTORE_FILE}" ]; then
+    echo "Keystore file not present!"
+    sleep_forever
 fi
+RAIDEN_NETWORK_ID=mainnet
+# Convert keystore address to EIP-55 format
+RAW_ADDRESS=$(cat ${KEYSTORE_FILE} | jq -r .address)
+RAIDEN_ADDRESS=$(echo -n ${RAW_ADDRESS} | eip55-checksum)
+export RAIDEN_ADDRESS
+
+echo "${RAIDEN_KEYSTORE_PASSWORD}" >/root/.raiden/.password
+
+# Generate config file from template
+envsubst <config.toml.template >config.toml
+
+echo "Starting Raiden node for: ${RAIDEN_ADDRESS}"
+exec raiden --config-file ./config.toml
